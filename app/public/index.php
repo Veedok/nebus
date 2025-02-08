8ef6cfb00428e507fc65f63930ef4d08be7e869c
Veedok <konakov88@gmail.com>
2025-02-08 22:22:49 +0300

    add Organization

--- app/public/index.php
+++ app/public/index.php
@@ -3,8 +3,6 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-$c = \Doctrine\Persistence\ObjectManager::class;
-$t = $c->getRepository('City');
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
